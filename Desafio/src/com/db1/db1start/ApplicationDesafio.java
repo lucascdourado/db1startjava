fd9fa9c891f7639b061721c25da7eec9bdb57a84
Lucas Dourado <lucascdourado@gmail.com>
2019-11-12 19:12:49 -0300

    desafio numeros ímpares sem laço de repetição

---
+++ Desafio/src/com/db1/db1start/ApplicationDesafio.java
@@ -0,0 +1,17 @@
+package com.db1.db1start;
+
+public class ApplicationDesafio {
+	
+	public Integer impar(Integer numero1) {
+		if ((numero1)%2 == 1) {
+			Integer resto;
+			resto = (numero1-1)/2;
+			Integer resp;
+			resp = resto+1;
+			return resp;
+		} return numero1/2;
+		
+	}
+
+}
+
---
+++ Desafio/src/com/db1/db1start/Desafio.java
@@ -0,0 +1,34 @@
+package com.db1.db1start;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class Desafio {
+	@Test
+	public void desafioImpar(){
+		ApplicationDesafio application = new ApplicationDesafio();
+		Integer expected = 13;
+		Integer response = application.impar(26);
+		Assert.assertEquals(expected, response);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
