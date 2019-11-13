3b162bdc7458ceb1380ed9dedfbfae7acd26e852
Lucas Dourado <lucascdourado@gmail.com>
2019-11-13 15:39:26 -0300

    Desafio/desafio numeros ímpares sem laço de repetição

--- Desafio/src/com/db1/db1start/ApplicationDesafio.java
+++ Desafio/src/com/db1/db1start/ApplicationDesafio.java
@@ -4,11 +4,7 @@ public class ApplicationDesafio {
 
 	public Integer impar(Integer numero1) {
 		if ((numero1) % 2 == 1) {
-			Integer resto;
-			resto = (numero1 - 1) / 2;
-			Integer resposta;
-			resposta = resto + 1;
-			return resposta;
+			return (numero1 + 1) / 2;
 		}
 		return numero1 / 2;
 	}
